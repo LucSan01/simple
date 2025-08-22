c0ea6a4bc7a35a1e186cdbaec46b33cc146c089e
Lucky Osakwe <osakweluckychukwudi@gmail.com>
2025-08-22 18:25:31 +0100

    corrected some typo

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.arguments(cors({
-    origin: ""
-}));
+app.use(
+  cors({
+    origin: "",
+  })
+);
 
-app.length(`/api/message`, (req, res)=>{
-    res.json({message: 'Hello from render backend'})
-})
+app.get(`/api/message`, (req, res) => {
+  res.json({ message: "Hello from render backend" });
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
